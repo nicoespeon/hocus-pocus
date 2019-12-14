46520b3b6938d80892a13cb96783beb6582a31d4
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-12-14 10:32:31 -0500

    Test when code should not be updated

--- src/create-function.test.ts
+++ src/create-function.test.ts
@@ -1,4 +1,4 @@
-import { Selection, Position } from "./editor";
+import { Selection, Position, Code } from "./editor";
 import { determineModification } from "./modification";
 
 // TODO: handle params
@@ -19,3 +19,27 @@ function readCode() {
   const expectedPosition = new Position(1, 0);
   expect(update).toBeCalledWith(expectedCode, expectedPosition);
 });
+
+it("should not update code if call expression is already declared", () => {
+  const code = `readCode();
+
+function readCode() {}`;
+  const selection = Selection.cursorAt(0, 0);
+
+  shouldNotUpdateCodeFor(code, selection);
+});
+
+it("should not update code if selection is not on call expression", () => {
+  const code = `const hello = "world";
+readCode();`;
+  const selection = Selection.cursorAt(0, 0);
+
+  shouldNotUpdateCodeFor(code, selection);
+});
+
+function shouldNotUpdateCodeFor(code: Code, selection: Selection) {
+  const update = jest.fn();
+
+  const modification = determineModification(code, selection);
+  modification.execute(update);
+}

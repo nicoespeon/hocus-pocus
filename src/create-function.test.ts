f4c8ccd0f0da6a422cf811ced21d35ac6894de29
Nicolas Carlo <nicolas.carlo@busbud.com>
2019-12-14 10:52:18 -0500

    Extract shouldUpdateCodeFor()

--- src/create-function.test.ts
+++ src/create-function.test.ts
@@ -7,50 +7,60 @@ import { determineModification } from "./modification";
 it("should create function declaration from a call expression", () => {
   const code = "readCode();";
   const selection = Selection.cursorAt(0, 0);
-  const update = jest.fn();
 
-  const modification = determineModification(code, selection);
-  modification.execute(update);
-
-  const expectedCode = `
+  shouldUpdateCodeFor(code, selection, {
+    code: `
 function readCode() {
   // Implement
-}`;
-  const expectedPosition = new Position(1, 0);
-  expect(update).toBeCalledWith(expectedCode, expectedPosition);
+}`,
+    position: new Position(1, 0)
+  });
 });
 
 it("should create function declaration from a call expression assigned to a variable, cursor on call expression", () => {
   const code = "const code = readCode();";
   const selection = Selection.cursorAt(0, 13);
-  const update = jest.fn();
 
-  const modification = determineModification(code, selection);
-  modification.execute(update);
-
-  const expectedCode = `
+  shouldUpdateCodeFor(code, selection, {
+    code: `
 function readCode() {
   // Implement
-}`;
-  const expectedPosition = new Position(1, 0);
-  expect(update).toBeCalledWith(expectedCode, expectedPosition);
+}`,
+    position: new Position(1, 0)
+  });
 });
 
 it("should create function declaration from a call expression param of another call, cursor on call expression", () => {
   const code = "console.log(readCode());";
   const selection = Selection.cursorAt(0, 13);
+
+  shouldUpdateCodeFor(code, selection, {
+    code: `
+function readCode() {
+  // Implement
+}`,
+    position: new Position(1, 0)
+  });
+});
+
+function shouldUpdateCodeFor(
+  code: Code,
+  selection: Selection,
+  expected: Expected
+) {
   const update = jest.fn();
 
   const modification = determineModification(code, selection);
   modification.execute(update);
 
-  const expectedCode = `
-function readCode() {
-  // Implement
-}`;
-  const expectedPosition = new Position(1, 0);
-  expect(update).toBeCalledWith(expectedCode, expectedPosition);
-});
+  expect(update).toBeCalledWith(expected.code, expected.position);
+}
+
+type Expected = {
+  code: Code;
+  position: Position;
+  // name: string;
+};
 
 it("should not update code if call expression is already declared", () => {
   const code = `readCode();

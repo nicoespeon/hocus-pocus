a861fa9a3a14c7689ac17a9b2bc762ce3b5f4432
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-05-02 09:38:24 -0400

    Test we handle types on different lines

--- src/create-function-with-types/type-checker.test.ts
+++ src/create-function-with-types/type-checker.test.ts
@@ -20,3 +20,15 @@ it("should return any if type can't be inferred", () => {
 
   expect(type).toBe("any");
 });
+
+it("should return the type of a typed variable on a different line", () => {
+  const code = `let count: number
+let total: number
+let isActive: boolean`;
+  const position = new Position(2, 5);
+  const typeChecker = new TypeChecker(code);
+
+  const type = typeChecker.getTypeAt(position);
+
+  expect(type).toBe("boolean");
+});

5c7291976a24b45181994347b9b5c313e19995e1
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-05-20 23:37:54 -0400

    Instantiate type checker in constructor

--- src/create-function-with-types/create-function-with-types.ts
+++ src/create-function-with-types/create-function-with-types.ts
@@ -30,6 +30,13 @@ function createFunctionWithTypes(
 }
 
 class CreateFunctionWithTypes extends CreateFunction {
+  private typeChecker: TypeChecker;
+
+  constructor(match: Match, code: Code) {
+    super(match, code);
+    this.typeChecker = new TypeChecker(code);
+  }
+
   execute(update: Update) {
     update({
       code: `\n${this.modifier}function ${this.name}(${this.args})${this.returnType} {\n  ${this.body}\n}${this.after}`,
@@ -39,15 +46,13 @@ class CreateFunctionWithTypes extends CreateFunction {
   }
 
   protected get args(): string {
-    const typeChecker = new TypeChecker(this.code);
-
     return this.match.node.arguments
       .map((argument, i) => {
         const name = t.isIdentifier(argument) ? argument.name : `param${i + 1}`;
         const position = t.isSelectableNode(argument)
           ? Position.fromAST(argument.loc.start)
           : new Position(0, 0);
-        const type = typeChecker.getTypeAt(position);
+        const type = this.typeChecker.getTypeAt(position);
 
         return `\${${i + 1}:${name}}: ${type}`;
       })
@@ -58,9 +63,8 @@ class CreateFunctionWithTypes extends CreateFunction {
     const { parent } = this.match;
 
     if (t.isVariableDeclarator(parent) && t.isSelectableNode(parent)) {
-      const typeChecker = new TypeChecker(this.code);
       const position = Position.fromAST(parent.loc.start);
-      const type = typeChecker.getTypeAt(position);
+      const type = this.typeChecker.getTypeAt(position);
 
       return `: ${type}`;
     }

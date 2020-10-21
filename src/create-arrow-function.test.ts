be531e36b34a062519c31e320811e7849d81dd9d
Jens Palmqvist <jens.palmqvist@fora.se>
2020-10-21 12:05:19 +0200

    prettier issues

--- src/create-arrow-function.test.ts
+++ src/create-arrow-function.test.ts
@@ -1,73 +1,80 @@
-import { createArrowFunction } from './create-arrow-function';
-import { Selection, Position } from './editor';
-import { createShouldUpdateCodeFor, createShouldNotUpdateCodeFor } from './test-helpers';
+import { createArrowFunction } from "./create-arrow-function";
+import { Selection, Position } from "./editor";
+import {
+  createShouldUpdateCodeFor,
+  createShouldNotUpdateCodeFor
+} from "./test-helpers";
 
 const shouldUpdateCodeFor = createShouldUpdateCodeFor(createArrowFunction);
-const shouldNotUpdateCodeFor = createShouldNotUpdateCodeFor(createArrowFunction);
+const shouldNotUpdateCodeFor = createShouldNotUpdateCodeFor(
+  createArrowFunction
+);
 
-describe('Create arrow function declaration from a call expression', () => {
-  it('with nothing else', () => {
+describe("Create arrow function declaration from a call expression", () => {
+  it("with nothing else", () => {
     shouldUpdateCodeFor({
-      code: 'readCode();',
+      code: "readCode();",
       selection: Selection.cursorAt(0, 0),
       expectedSnippet: {
         code: `
 const readCode = () => {
   \${0:// Implement}
 }`,
+
         position: new Position(1, 0),
-        name: 'Create arrow function "readCode"',
-      },
+        name: 'Create arrow function "readCode"'
+      }
     });
   });
 
-  it('assigned to a variable', () => {
+  it("assigned to a variable", () => {
     shouldUpdateCodeFor({
-      code: 'const code = readCode();',
+      code: "const code = readCode();",
       selection: Selection.cursorAt(0, 13),
       expectedSnippet: {
         code: `
 const readCode = () => {
   \${0:return undefined;}
-}`,
-      },
+}`
+      }
     });
   });
 
-  it('assignment called with await', () => {
+  it("assignment called with await", () => {
     shouldUpdateCodeFor({
-      code: 'async function doSomethingAsync() { const code = await readCode(); }',
+      code:
+        "async function doSomethingAsync() { const code = await readCode(); }",
       selection: Selection.cursorAt(0, 60),
       expectedSnippet: {
         code: `
 const readCode = async () => {
   \${0:return undefined;}
-}`,
-      },
+}`
+      }
     });
   });
 
-  it('await without assignment', () => {
+  it("await without assignment", () => {
     shouldUpdateCodeFor({
-      code: 'async function doSomethingAsync() { await readCode(); }',
+      code: "async function doSomethingAsync() { await readCode(); }",
       selection: Selection.cursorAt(0, 45),
       expectedSnippet: {
         code: `
 const readCode = async () => {
   \${0:// Implement}
-}`,
-      },
+}`
+      }
     });
   });
 
-  it('param of another call', () => {
+  it("param of another call", () => {
     shouldUpdateCodeFor({
-      code: 'console.log(readCode());',
-      selection: Selection.cursorAt(0, 13),
+      code: "console.log(readCode());",
+      selection: Selection.cursorAt(0, 13)
     });
   });
 
-  it('with assigned value referenced later', () => {
+  it("with assigned value referenced later", () => {
     shouldUpdateCodeFor({
       code: `const code = readCode();
 write(code);`,
@@ -78,8 +85,8 @@ const readCode = () => {
   \${0:return undefined;}
 }
 
-`,
-      },
+`
+      }
     });
   });
 
@@ -94,8 +101,8 @@ write(code);`,
 const readCode = () => {
   \${0:return undefined;}
 }
-`,
-      },
+`
+      }
     });
   });
 
@@ -111,21 +118,21 @@ write(code);`,
         code: `
 const readCode = () => {
   \${0:return undefined;}
-}`,
-      },
+}`
+      }
     });
   });
 
-  it('with other function declarations in the code', () => {
+  it("with other function declarations in the code", () => {
     shouldUpdateCodeFor({
       code: `readCode();
 
 function write(code) {}`,
-      selection: Selection.cursorAt(0, 0),
+      selection: Selection.cursorAt(0, 0)
     });
   });
 
-  it('with params', () => {
+  it("with params", () => {
     shouldUpdateCodeFor({
       code: `readCode(selection, "hello", 12);`,
       selection: Selection.cursorAt(0, 0),
@@ -134,12 +141,12 @@ function write(code) {}`,
 const readCode = (\${1:selection}, \${2:param2}, \${3:param3}) => {
   \${0:// Implement}
 }`,
-        name: 'Create arrow function "readCode"',
-      },
+        name: 'Create arrow function "readCode"'
+      }
     });
   });
 
-  it('nested in expression statements', () => {
+  it("nested in expression statements", () => {
     shouldUpdateCodeFor({
       code: `it("should read code", () => {
   const code = readCode();
@@ -153,33 +160,33 @@ const readCode = (\${1:selection}, \${2:param2}, \${3:param3}) => {
 const readCode = () => {
   \${0:return undefined;}
 }`,
-        position: new Position(5, 0),
-      },
+        position: new Position(5, 0)
+      }
     });
   });
 });
 
-it('should not update code if call expression is already declared', () => {
+it("should not update code if call expression is already declared", () => {
   shouldNotUpdateCodeFor({
     code: `readCode();
 
 function readCode() {}`,
-    selection: Selection.cursorAt(0, 0),
+    selection: Selection.cursorAt(0, 0)
   });
 });
 
-it('should not update code if selection is not on call expression', () => {
+it("should not update code if selection is not on call expression", () => {
   shouldNotUpdateCodeFor({
     code: `const hello = "world";
 readCode();`,
-    selection: Selection.cursorAt(0, 0),
+    selection: Selection.cursorAt(0, 0)
   });
 });
 
-it('should not update code if declared in a const', () => {
+it("should not update code if declared in a const", () => {
   shouldNotUpdateCodeFor({
     code: `const readCode = () => "hello";
 readCode();`,
-    selection: Selection.cursorAt(1, 0),
+    selection: Selection.cursorAt(1, 0)
   });
 });

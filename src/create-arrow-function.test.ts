9406217295065ec199ecab8032fe6c6ad89d1db4
Jens Palmqvist <jens.palmqvist@fora.se>
2020-10-21 11:07:52 +0200

    added create arrow function functionality

---
+++ src/create-arrow-function.test.ts
@@ -0,0 +1,191 @@
+import { createArrowFunction } from "./create-arrow-function";
+import { Selection, Position } from "./editor";
+import {
+  createShouldUpdateCodeFor,
+  createShouldNotUpdateCodeFor
+} from "./test-helpers";
+
+const shouldUpdateCodeFor = createShouldUpdateCodeFor(createArrowFunction);
+const shouldNotUpdateCodeFor = createShouldNotUpdateCodeFor(
+  createArrowFunction
+);
+
+describe("create function declaration from a call expression", () => {
+  it("with nothing else", () => {
+    shouldUpdateCodeFor({
+      code: "readCode();",
+      selection: Selection.cursorAt(0, 0),
+      expectedSnippet: {
+        code: `
+const readCode = () => {
+  \${0:// Implement}
+}`,
+        position: new Position(1, 0),
+        name: 'Create function "readCode"'
+      }
+    });
+  });
+
+  it("assigned to a variable", () => {
+    shouldUpdateCodeFor({
+      code: "const code = readCode();",
+      selection: Selection.cursorAt(0, 13),
+      expectedSnippet: {
+        code: `
+const readCode = () => {
+  \${0:return undefined;}
+}`
+      }
+    });
+  });
+
+  it("assignment called with await", () => {
+    shouldUpdateCodeFor({
+      code:
+        "async function doSomethingAsync() { const code = await readCode(); }",
+      selection: Selection.cursorAt(0, 60),
+      expectedSnippet: {
+        code: `
+const readCode = async () => {
+  \${0:return undefined;}
+}`
+      }
+    });
+  });
+
+  it("await without assignment", () => {
+    shouldUpdateCodeFor({
+      code: "async function doSomethingAsync() { await readCode(); }",
+      selection: Selection.cursorAt(0, 45),
+      expectedSnippet: {
+        code: `
+const readCode = async () => {
+  \${0:// Implement}
+}`
+      }
+    });
+  });
+
+  it("param of another call", () => {
+    shouldUpdateCodeFor({
+      code: "console.log(readCode());",
+      selection: Selection.cursorAt(0, 13)
+    });
+  });
+
+  it("with assigned value referenced later", () => {
+    shouldUpdateCodeFor({
+      code: `const code = readCode();
+write(code);`,
+      selection: Selection.cursorAt(0, 13),
+      expectedSnippet: {
+        code: `
+const readCode = () => {
+  \${0:return undefined;}
+}
+
+`
+      }
+    });
+  });
+
+  it("doesn't add unnecessary blank lines (1 blank line in-between)", () => {
+    shouldUpdateCodeFor({
+      code: `const code = readCode();
+
+write(code);`,
+      selection: Selection.cursorAt(0, 13),
+      expectedSnippet: {
+        code: `
+const readCode = () => {
+  \${0:return undefined;}
+}
+`
+      }
+    });
+  });
+
+  it("doesn't add unnecessary blank lines (2+ blank lines in-between)", () => {
+    shouldUpdateCodeFor({
+      code: `const code = readCode();
+
+
+
+write(code);`,
+      selection: Selection.cursorAt(0, 13),
+      expectedSnippet: {
+        code: `
+const readCode = () => {
+  \${0:return undefined;}
+}`
+      }
+    });
+  });
+
+  it("with other function declarations in the code", () => {
+    shouldUpdateCodeFor({
+      code: `readCode();
+
+function write(code) {}`,
+      selection: Selection.cursorAt(0, 0)
+    });
+  });
+
+  it("with params", () => {
+    shouldUpdateCodeFor({
+      code: `readCode(selection, "hello", 12);`,
+      selection: Selection.cursorAt(0, 0),
+      expectedSnippet: {
+        code: `
+const readCode = (\${1:selection}, \${2:param2}, \${3:param3}) => {
+  \${0:// Implement}
+}`,
+        name: 'Create function "readCode"'
+      }
+    });
+  });
+
+  it("nested in expression statements", () => {
+    shouldUpdateCodeFor({
+      code: `it("should read code", () => {
+  const code = readCode();
+
+  expect(code).toBe("hello");
+});
+`,
+      selection: Selection.cursorAt(1, 15),
+      expectedSnippet: {
+        code: `
+const readCode = () => {
+  \${0:return undefined;}
+}`,
+        position: new Position(5, 0)
+      }
+    });
+  });
+});
+
+it("should not update code if call expression is already declared", () => {
+  shouldNotUpdateCodeFor({
+    code: `readCode();
+
+function readCode() {}`,
+    selection: Selection.cursorAt(0, 0)
+  });
+});
+
+it("should not update code if selection is not on call expression", () => {
+  shouldNotUpdateCodeFor({
+    code: `const hello = "world";
+readCode();`,
+    selection: Selection.cursorAt(0, 0)
+  });
+});
+
+it("should not update code if declared in a const", () => {
+  shouldNotUpdateCodeFor({
+    code: `const readCode = () => "hello";
+readCode();`,
+    selection: Selection.cursorAt(1, 0)
+  });
+});
---
+++ src/create-arrow-function.ts
@@ -0,0 +1,90 @@
+import { Modification, Update, NoModification } from "./modification";
+import { Position, Selection, Code, isFilled } from "./editor";
+import * as t from "./ast";
+
+export { createArrowFunction };
+export { Match, isMatch, CreateArrowFunction };
+
+function createArrowFunction(code: Code, selection: Selection): Modification {
+  let match: Match | undefined;
+  t.traverseCode(code, {
+    CallExpression(path) {
+      if (!selection.isInsidePath(path)) return;
+      if (!isMatch(path)) return;
+      if (t.isDeclared(path.node.callee, path)) return;
+
+      match = path;
+    }
+  });
+
+  if (!match) {
+    return new NoModification();
+  }
+
+  return new CreateArrowFunction(match, code);
+}
+
+function isMatch(path: t.NodePath<t.CallExpression>): path is Match {
+  return t.isIdentifier(path.node.callee);
+}
+
+type Match = t.NodePath<
+  t.Selectable<t.CallExpression> & { callee: t.Identifier }
+>;
+
+class CreateArrowFunction implements Modification {
+  constructor(protected match: Match, protected code: Code) {}
+
+  execute(update: Update) {
+    update({
+      code: `\nconst ${this.name} = ${this.modifier}(${this.args}) => {\n  ${this.body}\n}${this.after}`,
+      position: this.position,
+      name: `Create arrow function "${this.name}"`
+    });
+  }
+
+  protected get modifier(): string {
+    if (this.match.parentPath.isAwaitExpression()) {
+      return "async ";
+    }
+    return "";
+  }
+
+  protected get name(): string {
+    return this.match.node.callee.name;
+  }
+
+  protected get position(): Position {
+    const ancestor = t.topLevelAncestor(this.match);
+    return Position.fromAST(ancestor.node.loc.end)
+      .putAtNextLine()
+      .putAtStartOfLine();
+  }
+
+  protected get after(): string {
+    const codeAfterPosition = this.code.split("\n").slice(this.position.line);
+
+    if (isFilled(codeAfterPosition[0])) return "\n\n";
+    if (isFilled(codeAfterPosition[1])) return "\n";
+    return "";
+  }
+
+  protected get args(): string {
+    return this.match.node.arguments
+      .map((argument, i) =>
+        t.isIdentifier(argument) ? argument.name : `param${i + 1}`
+      )
+      .map((argument, i) => `\${${i + 1}:${argument}}`)
+      .join(", ");
+  }
+
+  protected get body(): string {
+    const parentPath = this.match.parentPath;
+    const isAwait = parentPath.isAwaitExpression();
+    const isReturned = isAwait
+      ? parentPath.parentPath.isVariableDeclarator()
+      : parentPath.isVariableDeclarator();
+    const body = isReturned ? "return undefined;" : "// Implement";
+    return `\${0:${body}}`;
+  }
+}
